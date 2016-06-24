7cb9d7e5ad19879f12e264b9d1baeab54b670430
Vikram Samanna Vijayaraghavan <vv1538@att.com>
2016-06-24 15:18:35 -0700

    Added greeting function.

--- hello_world.py
+++ hello_world.py
@@ -11,8 +11,11 @@ from __future__ import division
 def print_hello(name):
     print("Hello, {}!".format(name))
 
-
+def print_greeting(name):
+    print("Good morning, {}!".format(name))
+    
+    
 if __name__ == "__main__":
     print_hello("Sophia")
-
+    print_greeting("Rishi")
 
