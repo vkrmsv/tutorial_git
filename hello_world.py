915aaaba5530bfd7c64eafb29f580942b34d7f64
Vikram Samanna Vijayaraghavan <vv1538@att.com>
2016-06-24 14:35:52 -0700

    Initial commit.

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+hello_world
+
+"""
+
+
+from __future__ import division
+
+
+def print_hello(name):
+    print("Hello, {}!".format(name))
+
+
+if __name__ == "__main__":
+    print_hello("Rishi")
+
+
